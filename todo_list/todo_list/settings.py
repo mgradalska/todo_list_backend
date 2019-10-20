7657185d91d65dc00e0d54a474b5a4fdf4c81dea
marta <martagradalska@gmail.com>
2019-10-20 17:56:54 +0200

    CORS config

--- todo_list/todo_list/settings.py
+++ todo_list/todo_list/settings.py
@@ -17,9 +17,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -30,6 +32,12 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'todo_list.urls'
+
+CORS_ORIGIN_ALLOW_ALL = True
+
+ALLOWED_HOSTS = [
+    'localhost'
+]
 
 TEMPLATES = [
     {
